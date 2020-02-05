4084acf13d4dc4090e7b2f1cfbe0588efd135041
Estefany Suarez <laura.suarez@mail.mcgill.ca>
2020-02-05 14:50:38 -0500

    changes in dependencies on the setup file

--- reservoir/info.py
+++ reservoir/info.py
@@ -8,13 +8,13 @@ DESCRIPTION = 'A pythonic reservoir toolbox'
 #DOWNLOAD_URL = 'http://github.com/rmarkello/pyls'
 
 INSTALL_REQUIRES = [
-    'numpy==1.17.0',
-    'scipy==1.1.0',
+    'numpy>=1.17',
+    'scipy>=1.1',
     'scikit-learn==0.19.1',
     'pandas==0.23.0',
-    'matplotlib==3.0.1',
+    'matplotlib',
     'netneurotools',
-    'seaborn==0.8.1',
+    'seaborn>=0.8',
     'bctpy==0.5.0',
     'MDP==3.5',
     'tqdm==4.28.1',
